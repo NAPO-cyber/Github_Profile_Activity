65fccf37b65c3d304dbe1418687203fe069cc8b3
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-03 09:18:38 +0530

    DI added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("enter username: ");
-        String username = sc.nextLine();
+    private final GithubClient client;
+
+    public Main(GithubClient client) {
+        this.client = client;
+    }
+
+    public void run() {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("enter username: ");
+            String username = sc.nextLine();
+
+            User user = client.getUser(username);
+
+            if (user != null) {
+                System.out.println("username: " + user.login);
+                System.out.println("bio: " + user.bio);
+                System.out.println("Followers: " + user.followers);
+                System.out.println("Following: " + user.following);
+                System.out.println("Public repos: " + user.public_repos);
+            }
+        }
+
+    public static void main (String[]args){
 
         GithubClient client = new GithubClient();
-        User user = client.getUser(username);
 
-
-        if (user != null) {
-            System.out.println("username: " + user.login);
-            System.out.println("bio: " + user.bio);
-            System.out.println("Followers: " + user.followers);
-            System.out.println("Following: " + user.following);
-            System.out.println("Public repos: " + user.public_repos);
-        }
+        Main app = new Main(client);
+        app.run();
     }
 }
