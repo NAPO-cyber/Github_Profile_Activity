ece4f8681a747ef85ecb5c843469a82f5a1e56bd
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-03 12:04:57 +0530

    added app configuration

---
+++ src/main/java/com/gopu/github/AppConfig.java
@@ -0,0 +1,10 @@
+package com.gopu.github;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.gopu.github")
+public class AppConfig {
+}
--- src/main/java/com/gopu/github/Main.java
+++ src/main/java/com/gopu/github/Main.java
@@ -35,7 +35,7 @@ public class Main {
 
     public static void main (String[]args){
 
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.gopu.github");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         Main app = context.getBean(Main.class);
         app.run();
