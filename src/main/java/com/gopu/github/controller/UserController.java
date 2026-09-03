ff1783b8b44548e45e3c124cf19463ee82a65fbb
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-03 21:04:30 +0530

    endpoint connected to react

--- src/main/java/com/gopu/github/controller/UserController.java
+++ src/main/java/com/gopu/github/controller/UserController.java
@@ -2,6 +2,7 @@ package com.gopu.github.controller;
 
 import com.gopu.github.model.User;
 import com.gopu.github.service.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Scanner;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserService service;
