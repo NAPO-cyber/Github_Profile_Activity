33e84cceb5767585ec55cca177c625602b581eab
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-03 08:52:33 +0530

    user info to fetch

---
+++ src/main/java/User.java
@@ -0,0 +1,12 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+
+    public String login;
+    public String bio;
+    public int followers;
+    public int following;
+    public int public_repos;
+
+}
