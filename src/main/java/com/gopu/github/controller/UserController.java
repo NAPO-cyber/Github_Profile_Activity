3f60099bb23157dfc03257788fe32a13f2bdac3d
NAPO-cyber <gopalverma2503@gmail.com>
2026-09-09 02:39:18 +0530

    run() rmved as app run control changed

--- src/main/java/com/gopu/github/controller/UserController.java
+++ src/main/java/com/gopu/github/controller/UserController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Scanner;
-
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
@@ -30,21 +28,4 @@ public class UserController {
         return service.getActivity(username);
     }
 
-    public void run() {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("enter username: ");
-        String username = sc.nextLine();
-
-        User user = service.getUser(username);
-
-        if (user != null) {
-            System.out.println("username: " + user.login);
-            System.out.println("bio: " + user.bio);
-            System.out.println("Followers: " + user.followers);
-            System.out.println("Following: " + user.following);
-            System.out.println("Public repos: " + user.public_repos);
-        }
-    }
 }
